91d8fe1ab82f2c6f8eac1d9576d0404db70e1d46
Aliia <26248907+aliiae@users.noreply.github.com>
2020-05-30 16:05:51 +0100

    Add SQL queries

--- sql_queries.py
+++ sql_queries.py
@@ -1,31 +1,67 @@
 # DROP TABLES
 
-songplay_table_drop = "DROP TABLE IF EXISTS songplay;"
-user_table_drop = "DROP TABLE IF EXISTS user;"
-song_table_drop = "DROP TABLE IF EXISTS song;"
-artist_table_drop = "DROP TABLE IF EXISTS artist;"
-time_table_drop = "DROP TABLE IF EXISTS table;"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
-CREATE TABLE IF NOT EXISTS songplay (songplay_id int, start_time int, user_id int, level varchar, song_id int, artist_id int, session_id int, location varchar, user_agent varchar);
+CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id int, 
+    start_time int,
+    user_id int, 
+    level varchar,
+    song_id int, 
+    artist_id int,
+    session_id int, 
+    location varchar,
+    user_agent varchar
+);
 """)
 
 user_table_create = ("""
-CREATE TABLE IF NOT EXISTS user (user_id int, first_name varchar, last_name varchar, gender varchar, level varchar);
+CREATE TABLE IF NOT EXISTS users (
+    user_id int,
+    first_name varchar,
+    last_name varchar,
+    gender varchar,
+    level varchar
+);
 """)
 
 song_table_create = ("""
-CREATE TABLE IF NOT EXISTS song (song_id int, title varchar, artist_id int, year int, duration int);
+CREATE TABLE IF NOT EXISTS songs (
+    song_id int,
+    title varchar,
+    artist_id int,
+    year int,
+    duration int
+);
 """)
 
 artist_table_create = ("""
-CREATE TABLE IF NOT EXISTS artist (artist_id int, name varchar, location varchar, latitude int, longitude int)
+CREATE TABLE IF NOT EXISTS artists (
+    artist_id int,
+    name varchar,
+    location varchar,
+    latitude int,
+    longitude int
+)
 """)
 
 time_table_create = ("""
-CREATE TABLE IF NOT EXISTS time (start_time int, hour int, day int, week int, month int, year int, weekday varchar);
+CREATE TABLE IF NOT EXISTS time (
+    start_time int,
+    hour int,
+    day int,
+    week int,
+    month int,
+    year int,
+    weekday varchar
+);
 """)
 
 # INSERT RECORDS
@@ -42,7 +78,6 @@ song_table_insert = ("""
 artist_table_insert = ("""
 """)
 
-
 time_table_insert = ("""
 """)
 
@@ -53,5 +88,7 @@ song_select = ("""
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [songplay_table_create, user_table_create, song_table_create,
+                        artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop,
+                      time_table_drop]
